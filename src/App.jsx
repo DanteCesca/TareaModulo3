14ba49cc578379f9539fd81e9e3a7250e8d026d6
DanteCesca <dantecesca597@gmail.com>
2023-12-20 20:51:33 -0300

    feat(App.jsx): se trabajo en las cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from "react"
+import React, { useState, useEffect } from "react"
 import Navbar from "./components/general/navbar/Navbar"
 import Cards from "./components/specific/cards/Cards"
 import axios from "axios"
@@ -20,16 +20,14 @@ function App() {
     handleRequest();
   }, []);
   
-  
+  const createCard = () => characters.map((character) => <Cards key={character.id} character={character} />);
   
   return (
     <>
       <Navbar/>
       <div className="container-fluid">
         <div className="row justify-content-around">
-          {
-            characters.map((character) => <Cards key={character.id} character={character}/>)
-          }
+          {createCard()}
         </div>
       </div>
     </>
--- src/components/specific/cards/Cards.jsx
+++ src/components/specific/cards/Cards.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Cards = ({character: {name, image, species, status, location}}) => {
-
+const Cards = ({character: {name, image, species, status, location: { name: locationName }}}) => {
+ console.log(location)
     return (    
-    <div className="card col-sm-10 col-md-5 col-lg-3" style="width: 18rem;">
+    <div className="card col-sm-10 col-md-5 col-lg-3">
         <img src={image} className="card-img-top" alt="..."/>
         <div className="card-body">
             <h5 className="card-title">{name}</h5>
@@ -11,7 +11,7 @@ const Cards = ({character: {name, image, species, status, location}}) => {
         <ul className="list-group list-group-flush">
             <li className="list-group-item">Especie: {species}</li>
             <li className="list-group-item">Estatus: {status}</li>
-            <li className="list-group-item">Visto en: {location}</li>
+            <li className="list-group-item">Visto en: {locationName}</li>
         </ul>
         <div className="card-body">
             <a href="#" className="card-link">Ver mas</a>
